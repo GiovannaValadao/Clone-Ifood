816eb2fb1cfe06ccc9580ddb6c261ec100271292
GiovannaValadao <giovannnavsousa04@gmail.com>
2022-02-08 21:19:28 -0300

    add suggestions

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import HomeHeader from "./components/header/HomeHeader.jsx";
 import HomeHeaderMobile from "./components/headerMobile/HomeHeaderMobile.jsx";
 import VoucherHeader from "./components/voucherHeader/VoucherHeader.jsx";
-
 import "./styles/global.css";
+
 function App() {
   return (
     <div className="App">
