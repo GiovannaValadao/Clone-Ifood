23bcad6cc8f6c1eb13d608064cc57ff5653c131f
GiovannaValadao <giovannnavsousa04@gmail.com>
2022-03-13 12:57:22 -0300

    add alt for images

--- src/components/carouselCategories/CarouselCategories.jsx
+++ src/components/carouselCategories/CarouselCategories.jsx
@@ -23,6 +23,7 @@ function CarouselCategories() {
             <img
               className="carousel-categories__container-list-image"
               src={Groceries}
+              alt="Ilustração de Mercado"
             />
             <p>Mercado</p>
           </li>
@@ -30,6 +31,7 @@ function CarouselCategories() {
             <img
               className="carousel-categories__container-list-image"
               src={Acai}
+              alt="Ilustração de Açaí"
             />
             <p>Açaí</p>
           </li>{" "}
@@ -37,6 +39,7 @@ function CarouselCategories() {
             <img
               className="carousel-categories__container-list-image"
               src={SweetAndCakes}
+              alt="Ilustração de Doces e Bolos"
             />
             <p>Doces & Bolos</p>
           </li>{" "}
@@ -44,6 +47,7 @@ function CarouselCategories() {
             <img
               className="carousel-categories__container-list-image"
               src={Promotions}
+              alt="Ilustração de Promoção"
             />
             <p>Promoções</p>
           </li>{" "}
@@ -51,6 +55,7 @@ function CarouselCategories() {
             <img
               className="carousel-categories__container-list-image"
               src={Snacks}
+              alt="Ilustração de Lanches"
             />
             <p>Lanches</p>
           </li>
@@ -58,6 +63,7 @@ function CarouselCategories() {
             <img
               className="carousel-categories__container-list-image"
               src={Salted}
+              alt="Ilustração de Salgados"
             />
             <p>Salgados</p>
           </li>
@@ -65,6 +71,7 @@ function CarouselCategories() {
             <img
               className="carousel-categories__container-list-image"
               src={IceCream}
+              alt="Ilustração de sorvete"
             />
             <p>Sorvetes</p>
           </li>
@@ -72,6 +79,7 @@ function CarouselCategories() {
             <img
               className="carousel-categories__container-list-image"
               src={Japan}
+              alt="Ilustração de comida japonesa"
             />
             <p>Japonesa</p>
           </li>
@@ -79,6 +87,7 @@ function CarouselCategories() {
             <img
               className="carousel-categories__container-list-image"
               src={Pizza}
+              alt="Ilustração de pizza"
             />
             <p>Pizza</p>
           </li>
@@ -86,6 +95,7 @@ function CarouselCategories() {
             <img
               className="carousel-categories__container-list-image"
               src={Bakery}
+              alt="Ilustração de Pães"
             />
             <p>Padaria</p>
           </li>
@@ -93,6 +103,7 @@ function CarouselCategories() {
             <img
               className="carousel-categories__container-list-image"
               src={Italian}
+              alt="Ilustração de comida italiana"
             />
             <p>Italiana</p>
           </li>
@@ -100,6 +111,7 @@ function CarouselCategories() {
             <img
               className="carousel-categories__container-list-image"
               src={Brazilian}
+              alt="Ilustração de comida brasileira"
             />
             <p>Brasileira</p>
           </li>
@@ -107,6 +119,7 @@ function CarouselCategories() {
             <img
               className="carousel-categories__container-list-image"
               src={Healthy}
+              alt="Ilustração de comida saúdavel"
             />
             <p>Saudável</p>
           </li>
@@ -114,6 +127,7 @@ function CarouselCategories() {
             <img
               className="carousel-categories__container-list-image"
               src={Chinese}
+              alt="Ilustração de comida chinesa"
             />
             <p>Chinesa</p>
           </li>
