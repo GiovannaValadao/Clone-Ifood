d3cec6971a42c9749435358ece1c2072bce5b983
GiovannaValadao <giovannnavsousa04@gmail.com>
2022-03-24 20:06:09 -0300

    wiip

---
+++ src/components/bannerCardstack/BannerCardstack.jsx
@@ -0,0 +1,74 @@
+import All from "../../static/images/all.png";
+import "./bannerCardstack.css";
+
+function BannerCardstack() {
+  return (
+    <div class="banner-cardstack">
+      <div class="banner-cardstack__container">
+        <ul class="banner-cardstack__container-list">
+          <li class="banner-cardstack__container-list-item">
+            {" "}
+            <img
+              className="image"
+              src={All}
+              alt="Ilustração de Mercado"
+              width="392px"
+              height="183px"
+            />
+          </li>
+          <li class="banner-cardstack__container-list-item">
+            {" "}
+            <img
+              className="image"
+              src={All}
+              alt="Ilustração de Mercado"
+              width="392px"
+              height="183px"
+            />
+          </li>
+          <li class="banner-cardstack__container-list-item">
+            {" "}
+            <img
+              className="image"
+              src={All}
+              alt="Ilustração de Mercado"
+              width="392px"
+              height="183px"
+            />
+          </li>
+          <li class="banner-cardstack__container-list-item">
+            {" "}
+            <img
+              className="image"
+              src={All}
+              alt="Ilustração de Mercado"
+              width="392px"
+              height="183px"
+            />
+          </li>
+          <li class="banner-cardstack__container-list-item">
+            {" "}
+            <img
+              className="image"
+              src={All}
+              alt="Ilustração de Mercado"
+              width="392px"
+              height="183px"
+            />
+          </li>
+          <li class="banner-cardstack__container-list-item">
+            {" "}
+            <img
+              className="image"
+              src={All}
+              alt="Ilustração de Mercado"
+              width="392px"
+              height="183px"
+            />
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+export default BannerCardstack;
